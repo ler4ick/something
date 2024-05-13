9f151d530077e4577e8ef76879e41c9f71bbaf30
ler4ick <skorlera516@gmail.com>
2024-05-13 21:58:15 +0300

    смена имен при переключении между диалогами

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { persons } from '../persons';
+import { ChatService } from '../chat.service';
 
 @Component({
   selector: 'app-top-bar',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrl: './top-bar.component.scss'
 })
 export class TopBarComponent {
+
+  selectedChatId: number | null = null;
+  name: string = 'Не';
+  lastName: string = 'выбран';
+
+  constructor(private chatService: ChatService) { }
+
+  ngOnInit() {
+    this.chatService.selectedChatId$.subscribe(chatId => {
+      this.selectedChatId = chatId;
+      this.loadUserName();
+    });
+  }
+
+  loadUserName() {
+    if (this.selectedChatId !== null) {
+      const user = persons.find(person => person.id === this.selectedChatId);
+      if (user) {
+        this.lastName = user.lastname;
+        this.name = user.name;
+      } else {
+        this.lastName = '';
+        this.name = '';
+      }
+    }
+  }
 
 }
